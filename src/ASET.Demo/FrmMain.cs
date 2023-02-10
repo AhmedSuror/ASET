398ac59cee14e70672da2c4a16e263cf1bbf237b
Ahmed H. Suror <ahmedsuror@live.com>
2023-02-11 01:49:43 +0200

    Add project files.

---
+++ src/ASET.Demo/FrmMain.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Windows.Forms;
+
+namespace ASET.Demo
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ src/ASET.Demo/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Windows.Forms;
+
+namespace ASET.Demo
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+#if NET6_0_OR_GREATER
+            ApplicationConfiguration.Initialize();
+#endif
+            Application.Run(new FrmMain());
+        }
+    }
+}
